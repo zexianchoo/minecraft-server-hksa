4910c4b1197eb16bfa4c95d5eae6476c06dde035
Choo Ze Xian <choozexian@gmail.com>
2026-02-20 13:57:38 -0600

    feat: check url in cron

--- poll.sh
+++ poll.sh
@@ -4,6 +4,19 @@ git fetch
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
     bash make_backup.sh
     git pull
-    gadocker compose up -d
-    echo "Minecraft updated to latest Paper build at $(date)"
+    source .build-ver
+    HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}" -I -L \
+        -H "User-Agent: MinecraftUpdater/1.0" \
+        "${BUILD_URL}")
+
+    if [ "$HTTP_STATUS" -eq 200 ]; then
+        bash make_backup.sh
+        export BUILD_URL
+        docker compose up -d
+        
+        echo "Update to ${BUILD_URL} complete at $(date)"
+    else
+        echo "URL ${BUILD_URL} returned HTTP ${HTTP_STATUS}. are u sure its legit"
+        exit 1
+    fi
 fi
