2a0e94ccaec78d298a64d1ad651fb47b17bf3555
Choo Ze Xian <choozexian@gmail.com>
2026-02-20 14:00:22 -0600

    feat: make the cron only run on change to build ver

--- poll.sh
+++ poll.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
+source .build-ver
+OLD_URL=$BUILD_URL
+
 git fetch
 
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
     bash make_backup.sh
     git pull
     source .build-ver
-    HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}" -I -L \
-        -H "User-Agent: MinecraftUpdater/1.0" \
-        "${BUILD_URL}")
+    NEW_URL=$BUILD_URL
+    if [ "$OLD_URL" != "$NEW_URL" ]; then
 
-    if [ "$HTTP_STATUS" -eq 200 ]; then
-        bash make_backup.sh
-        export BUILD_URL
-        docker compose up -d
-        
-        echo "Update to ${BUILD_URL} complete at $(date)"
-    else
-        echo "URL ${BUILD_URL} returned HTTP ${HTTP_STATUS}. are u sure its legit"
-        exit 1
+        HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}" -I -L \
+            -H "User-Agent: MinecraftUpdater/1.0" \
+            "${BUILD_URL}")
+
+        if [ "$HTTP_STATUS" -eq 200 ]; then
+            bash make_backup.sh
+            export BUILD_URL
+            docker compose up -d
+            
+            echo "Update to ${BUILD_URL} complete at $(date)"
+        else
+            echo "URL ${BUILD_URL} returned HTTP ${HTTP_STATUS}. are u sure its legit"
+            exit 1
+        fi
     fi
 fi
